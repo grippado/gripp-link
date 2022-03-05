d0e5e232e5e7fda85d68079aa5e4dfbf09489b86
Gabriel Gripp <grippado@gmail.com>
2022-03-05 15:09:56 -0300

    add simple page

--- middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-export default function ({ store, redirect }: any) {
-  // If the user is not authenticated
-
-  if (!store.state.auth) {
-    return redirect('/login')
-  }
-}
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,31 @@
 <template>
-  <Tutorial />
+  <div id="login">
+    <p>gripp.link</p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
-  middleware: 'auth'
+  name: 'LoginPage'
 })
 </script>
+
+<style>
+  #login {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background: #15202b;
+    flex-direction: column;
+  }
+  #login p {
+    color: #fff;
+    font-size: 4rem;
+    margin-bottom: 18px;
+  }
+</style>
